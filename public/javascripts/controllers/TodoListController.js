5f108282aa99d750322cf4a2929de3b83454a4d8
Valeri Karpov <valkar207@gmail.com>
2013-07-25 14:07:19 -0400

    Step 8: add basic frontend for POST /todo.json

--- public/javascripts/controllers/TodoListController.js
+++ public/javascripts/controllers/TodoListController.js
@@ -1,10 +1,22 @@
-function TodoListController($scope) {
+function TodoListController($scope, $http) {
   $scope.todos = [];
+  $scope.newTodo = {
+    done : false,
+    due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+    description : ''
+  };
 
   $scope.doneFilter = { done : true };
   $scope.notDoneFilter = { done : false };
 
   $scope.setTodos = function(todos) {
     $scope.todos = todos;
+  };
+
+  $scope.addNewTodo = function() {
+    $http.post('/todo.json', $scope.newTodo).success(function(data) {
+      $scope.todos = data.todos;
+      $scope.newTodo.description = '';
+    });
   };
 }
