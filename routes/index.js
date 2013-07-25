9baed5a3546e13dd5d34e9ea6f4b298c644a923d
Valeri Karpov <valkar207@gmail.com>
2013-07-25 12:20:51 -0400

    Step 1: create sample data

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    todos : [
+      { description : "Buy eggs",
+        due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
+        done : false
+      },
+      { description : "Write next blog post",
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+        done : false
+      },
+      { description : "Build todo list app",
+        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+        done : true
+      },
+    ]
+  });
 };
