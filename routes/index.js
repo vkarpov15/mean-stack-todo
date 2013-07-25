67859d39e5f18e2b4762abc39064349c931e9c7e
Valeri Karpov <valkar207@gmail.com>
2013-07-25 13:51:02 -0400

    Step 7: refactor out todos for dependency injection

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', {
-    title: 'Express',
-    todos : [
-      { description : "Buy eggs",
-        due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
-        done : false
-      },
-      { description : "Write next blog post",
-        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
-        done : false
-      },
-      { description : "Build todo list app",
-        due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
-        done : true
-      },
-    ]
-  });
+exports.index = function(todos) {
+  return function(req, res){
+    res.render('index', {
+      title: 'Express',
+      todos : todos
+    });
+  };
 };
 
 exports.addTodo = function(todos) {
   return function(req, res) {
     todos.push(req.body);
     res.json({ todos : todos });
-  }
+  };
 };
