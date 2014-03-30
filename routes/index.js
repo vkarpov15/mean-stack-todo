eb840e58e82ae628bf2c4a92166a649400622b00
martypb <martypb@gmail.com>
2014-03-30 22:50:17 +1100

    Revert a1258e6..69407e8
    This rolls back to commit a1258e639f2fec74310a6a2755724e518616cfe1.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,53 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(Todo) {
+  return function(req, res) {
+    Todo.find({}, function(error, todos) {
+      res.render('index', {
+        title: 'Express',
+        todos : todos
+      });
+    });
+  };
+};
+
+exports.addTodo = function(Todo) {
+  return function(req, res) {
+    var todo = new Todo(req.body);
+    todo.save(function(error, todo) {
+      if (error || !todo) {
+        res.json({ error : error });
+      } else {
+        res.json({ todo : todo });
+      }
+    });
+  };
+};
+
+exports.get = function(Todo) {
+  return function(req, res) {
+    Todo.find({}, function(error, todos) {
+      res.json({ todos : todos });
+    });
+  }
+};
+
+exports.update = function(Todo) {
+  return function(req, res) {
+    Todo.findOne({ _id : req.params.id }, function(error, todo) {
+      if (error || !todo) {
+        res.json({ error : error });
+      } else {
+        todo.done = req.body.done;
+        todo.save(function(error, todo) {
+          if (error || !todo) {
+            res.json({ error : error });
+          } else {
+            res.json({ todo : todo });
+          }
+        });
+      }
+    });
+  }
 };
